935d088655758c1c55adedb6986fee00db4e692b
Sina3127 <sinaaminian3127@gmail.com>
2023-12-02 10:01:26 -0800

    fixing ckeditor

--- home/models.py
+++ home/models.py
@@ -1,65 +1,56 @@
-from ckeditor_uploader.fields import RichTextUploadingField
+from ckeditor.fields import RichTextField
 
 from django.db import models
 
 
 class Header(models.Model):
-    title = models.TextField(max_length=50)
-    paragraph = models.TextField(max_length=200)
+    title = RichTextField(max_length=50)
+    paragraph = RichTextField(max_length=200)
     img = models.ImageField(upload_to="uploads/")
-    body = RichTextUploadingField()
 
 
 class ProvideServices(models.Model):
     svg = models.ImageField(upload_to="uploads/")
-    title = models.TextField(max_length=50)
-    text = models.TextField(max_length=200)
-    body = RichTextUploadingField()
+    title = RichTextField(max_length=50)
+    text = RichTextField(max_length=200)
 
 
 class WorkExperience(models.Model):
-    title = models.TextField(max_length=50)
-    description = models.TextField(max_length=100)
+    title = RichTextField(max_length=50)
+    description = RichTextField(max_length=100)
     start_date = models.DateField()
     end_date = models.DateField(null=True, blank=True)
     link = models.TextField(max_length=100, null=True, blank=True)
-    body = RichTextUploadingField()
 
 
 class Education(models.Model):
-    title = models.TextField(max_length=50)
-    description = models.TextField(max_length=100)
+    title = RichTextField(max_length=50)
+    description = RichTextField(max_length=100)
     start_date = models.DateField()
     end_date = models.DateField(null=True, blank=True)
     link = models.TextField(max_length=100, null=True, blank=True)
-    body = RichTextUploadingField()
 
 
 class FreelanceClients(models.Model):
     logo = models.ImageField(upload_to="uploads/")
-    body = RichTextUploadingField()
 
 
 class Skills(models.Model):
-    text = models.TextField(max_length=30)
-    body = RichTextUploadingField()
+    text = RichTextField(max_length=30)
 
 
 class Tools(models.Model):
     svg = models.ImageField(upload_to="uploads/", validators=[])
-    body = RichTextUploadingField()
 
 
 class Stats(models.Model):
     projectsCompleted = models.IntegerField(default=0)
     designAwards = models.IntegerField(default=0)
-    YearsExperience = models.IntegerField(default=0)
+    yearsExperience = models.IntegerField(default=0)
     happyClients = models.IntegerField(default=0)
-    body = RichTextUploadingField()
 
 
 class ClientsFeedbacks(models.Model):
-    text = models.TextField(max_length=200)
+    text = RichTextField(max_length=200)
     img = models.ImageField(upload_to="uploads/")
-    name = models.TextField(max_length=100)
-    body = RichTextUploadingField()
+    name = RichTextField(max_length=100)
